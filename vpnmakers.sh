c140c9bffe54f2141322f58dbdb4045d5eb16a51
farhad <farhadcuber@gmail.com>
2019-07-15 18:34:12 +0430

    added disconnect and connect

--- vpnmakers.sh
+++ vpnmakers.sh
@@ -2,21 +2,33 @@
 
 source ./.config
 
+check_connection () {
+
+    PID=$(ps -a | grep openconnect | cut -d' ' -f1)
+    return $PID
+}
+
 if [ $# -gt 1 ]; then
     echo "wrong usage."
     echo "sudo ./vpnmakers.sh    --> for connection"
     echo "sudo ./vpnmakers.sh -d --> for disconnet"
     echo "sudo ./vpnmakers.sh -s --> to see status"
 fi
-check_connection () {
-    PID=$(ps -a | grep openconnect | cut -d' ' -f1)
-    if [[ $PID -eq '' ]]; then
-        echo 'not connected'
-    else
-        echo 'connected'
-    fi
-}
 
 if [ $# -eq 0 ]; then
     check_connection
+    if [[ $? -eq '' ]]; then
+        { echo $PASSWORD; echo 'yes';} | sudo openconnect --user=$USERNAME -b \
+        --passwd-on-stdin $SERVER
+    fi
+
+fi
+
+if [ $1 == '-d' ]; then
+    check_connection
+    PID=$?
+    if [[ $PID -ne '' ]]; then
+        sudo kill $PID
+    fi
+    echo 'disconnected'
 fi
