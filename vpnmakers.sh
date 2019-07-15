08dc462455cdb385c1e28ad28bef703dcfdb18cb
farhad <farhadcuber@gmail.com>
2019-07-15 15:22:49 +0430

    added check_connection func

--- vpnmakers.sh
+++ vpnmakers.sh
@@ -8,4 +8,15 @@ if [ $# -gt 1 ]; then
     echo "sudo ./vpnmakers.sh -d --> for disconnet"
     echo "sudo ./vpnmakers.sh -s --> to see status"
 fi
+check_connection () {
+    PID=$(ps -a | grep openconnect | cut -d' ' -f1)
+    if [[ $PID -eq '' ]]; then
+        echo 'not connected'
+    else
+        echo 'connected'
+    fi
+}
 
+if [ $# -eq 0 ]; then
+    check_connection
+fi
